9ad8c2c921056f982fba37e9e4dbe3589030b9fe
Alejandro Barroso <masbienmenosmal@gmail.com>
2023-05-30 12:09:28 -0300

    Estilos a la vista de Crear

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,62 @@
-<h1>Crea Tu Cuenta</h1>
+<div class="contenedor crear">
+
+<?php include_once __DIR__ . '/../templates/nombre-sitio.php' ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
+
+        <form action="/" class="formulario" method="POST">
+
+        <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input
+                    type="text"
+                    id="nombre"
+                    placeholder="Tu Nombre"
+                    name="nombre"
+                    value=""
+                />
+            </div>
+
+            <div class="campo">
+                <label for="email">E-mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu E-mail"
+                    name="email"
+                    value=""
+                />
+            </div>
+
+    
+            <div class="campo">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Tu Password"
+                    name="password"
+                    value=""
+                />
+            </div>
+            
+            <div class="campo">
+                <label for="password2">Confirmar Password</label>
+                <input
+                    type="password"
+                    id="password2"
+                    placeholder="Repite tu Password"
+                    name="password2"
+                    value=""
+                />
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/">Ya tienes una cuenta? Iniciar sesión.</a>
+            <a href="/olvide">Olvidaste tu password?</a>
+        </div>
+    </div> <!-- contenedor-sm -->
+</div>
