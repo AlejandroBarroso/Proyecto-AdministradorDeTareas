cae5e8cecb087d9f9ef825032748d106542b9ca2
Alejandro Barroso <masbienmenosmal@gmail.com>
2023-06-09 10:22:34 -0300

    validacion al Proyecto

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -22,39 +22,50 @@ class DashboardController {
         session_start();
         isAuth();
         $alertas = [];
-
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             $proyecto = new Proyecto($_POST);
-
             // Validar
             $alertas = $proyecto->validarProyecto();
-
             if(empty($alertas)) {
-
                 // Generar una url unica
                 $hash = md5(uniqid());
                 $proyecto->url = $hash;
-
                 // Almacenar el creador del proyecto
                 $proyecto->propietarioId = $_SESSION['id'];
-
                 // Guardar el proyecto
                 $proyecto->guardar();
-
                 // Redirecciionar
                 header('Location: /proyecto?id=' . $proyecto->url);
-
             }
-
         }
-
-
         $router->render('dashboard/crear-proyecto', [
             'alertas' => $alertas,
             'titulo' => 'Crear Proyecto'
+        ]);
+    }
+
+    public static function proyecto(Router $router) {
+        session_start();
+        isAuth();
+
+        $tokenURL = $_GET['id'];
+        if(!$tokenURL) header('Location: /dashboard');
+
+        // Revisa que la persona que visita el proyecto sea quien lo creó
+        $proyecto = Proyecto::where('url', $tokenURL);
+
+        if($proyecto->propietarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
 
         ]);
     }
+
+
+
 
     public static function perfil(Router $router) {
         session_start();
