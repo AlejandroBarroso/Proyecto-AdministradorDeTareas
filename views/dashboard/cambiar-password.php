367e714c3a2052f94e68bd15dd14caeac15f6415
Alejandro Barroso <masbienmenosmal@gmail.com>
2023-06-26 12:49:54 -0300

    Dark Mode

--- views/dashboard/cambiar-password.php
+++ views/dashboard/cambiar-password.php
@@ -1,10 +1,7 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
-
 <div class="contenedor-sm">
     <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
-
     <a href="/perfil" class="enlace">Volver al Perfil</a>
-
         <form action="/cambiar-password" class="formulario" method="POST">
             <div class="campo">
                 <label for="nombre">Password Actual</label>
@@ -33,5 +30,4 @@
             <input type="submit" value="Guardar Cambios">   
         </form>
 </div>
-
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,10 +1,7 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
-
 <div class="contenedor-sm">
     <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
-
     <a href="/cambiar-password" class="enlace">Cambiar Password</a>
-
         <form action="/perfil" class="formulario" method="POST">
             <div class="campo">
                 <label for="nombre">Nombre</label>
@@ -27,5 +24,4 @@
             <input type="submit" value="Guardar Cambios">   
         </form>
     </div>
-
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
